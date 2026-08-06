076b8731cf29c5b2b1b04b3049cd2c159275a60b
Steph <stephtruitt8@gmail.com>
2026-08-05 22:05:57 -0400

    Initial Commit

---
+++ 104-project-cohort 11/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  104-project-cohort 11
+//
+//  Created by Stephen Truitt on 8/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 104-project-cohort 11/_04_project_cohort_11App.swift
@@ -0,0 +1,17 @@
+//
+//  _04_project_cohort_11App.swift
+//  104-project-cohort 11
+//
+//  Created by Stephen Truitt on 8/5/26.
+//
+
+import SwiftUI
+
+@main
+struct _04_project_cohort_11App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
